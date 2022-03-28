e8e501711095cd40fa0e3232bea219b50cef8d7a
Kevin Arnaud <kevin.arnaud.addpro@gmail.com>
2022-03-28 15:25:50 +0200

    h2 view ok

--- src/main/java/com/example/demo/model/Conseiller.java
+++ src/main/java/com/example/demo/model/Conseiller.java
@@ -1,5 +1,22 @@
 package com.example.demo.model;
-
+import javax.persistence.*;
+import lombok.Data;
+@Data
+@Entity
+@Table(name = "Conseillers")
 public class Conseiller {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	@Column(name = "nom")
+	private String nom;
+	@Column(name = "prenom")
+	private String prenom;
+	@Column(name = "identifiant")
+	private String identifiant;
+	@Column(name = "mot_de_passe")
+	private String mot_de_passe;
 	
+//	@OneToMany(mappedBy = "client")
+//	private Client client;
 }
