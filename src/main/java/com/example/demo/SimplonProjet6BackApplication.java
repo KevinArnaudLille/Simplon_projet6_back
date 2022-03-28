ca38dac3db8f2193313526d2a8216b0960c3d6b1
Kevin Arnaud <kevin.arnaud.addpro@gmail.com>
2022-03-28 11:31:59 +0200

    first commit

---
+++ src/main/java/com/example/demo/SimplonProjet6BackApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplonProjet6BackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplonProjet6BackApplication.class, args);
+	}
+
+}
